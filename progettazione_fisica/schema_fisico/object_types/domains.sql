fe1977aecd67a5fa98cc3ff09a5a18ba37208ab6
ThatsSasi <102898641+ThatsSasi@users.noreply.github.com>
2023-10-25 16:57:30 +0200

    Update domains.sql
    doi domain

--- progettazione_fisica/schema_fisico/object_types/domains.sql
+++ progettazione_fisica/schema_fisico/object_types/domains.sql
@@ -1,3 +1,4 @@
 CREATE DOMAIN isbn AS CHAR(17) CHECK(VALUE LIKE '978-__-___-____-_');
 CREATE DOMAIN issn AS CHAR(9) CHECK(VALUE LIKE '____-____');
+CREATE DOMAIN doi AS TEXT CHECK(VALUE LIKE '^https:\/\/doi\.org\/10\.\d{4,}/[a-zA-Z0-9.-]+\/\d{4}\.\d{4}$')
 CREATE DOMAIN email_type AS VARCHAR CHECK(VALUE LIKE '%@%.%');
