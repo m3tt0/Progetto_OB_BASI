f869f8b0505433cdb6f05b182b90430ef519a2e5
alescetto <alescetto03@gmail.com>
2023-12-12 15:38:02 +0100

    refactoring e testing view

--- progettazione_fisica/DDL/views.sql
+++ progettazione_fisica/DDL/views.sql
@@ -1,40 +1,40 @@
-CREATE OR REPLACE FUNCTION LibriInNegozio(cur_negozio Negozio.partita_iva%type)
+CREATE OR REPLACE FUNCTION LibriInNegozio(negozio_richiesto Negozio.partita_iva%TYPE)
 RETURNS TABLE (isbn Libro.isbn%TYPE, titolo Libro.titolo%TYPE, costo Vendita.costo%TYPE, quantita Vendita.quantita%TYPE) AS
 $$
 BEGIN
     RETURN QUERY(
         SELECT l.isbn, l.titolo, v.costo, v.quantita
         FROM Vendita AS v JOIN Libro AS l ON v.libro = l.isbn
-        WHERE v.negozio = cur_negozio
+        WHERE v.negozio = negozio_richiesto
     );
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION RaccolteDiUtente(cur_richiedente Utente.username%type, cur_proprietario Utente.username%type)
+CREATE OR REPLACE FUNCTION RaccolteDiUtente(utente_richiedente Utente.username%TYPE, utente_richiesto Utente.username%TYPE)
 RETURNS TABLE (nome Raccolta.nome%TYPE) AS
 $$
 BEGIN
     RETURN QUERY(
         SELECT r.nome
         FROM Raccolta AS r
-        WHERE r.proprietario = cur_proprietario AND (cur_richiedente = cur_proprietario OR r.visibilita = 'pubblica')
+        WHERE r.proprietario = utente_richiesto AND (utente_richiedente = utente_richiesto OR r.visibilita = 'pubblica')
     );
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION LibriInRaccolta(cur_raccolta Raccolta.cod_raccolta%type, cur_utente Utente.username%type)
+CREATE OR REPLACE FUNCTION LibriInRaccolta(raccolta_richiesta Raccolta.cod_raccolta%TYPE, utente_richiedente Utente.username%TYPE)
 RETURNS TABLE (isbn Libro.isbn%TYPE, titolo Libro.titolo%TYPE) AS
 $$
 BEGIN
     RETURN QUERY(
         SELECT l.isbn, l.titolo
         FROM Libro AS l JOIN Libro_Contenuto_Raccolta AS lcr ON l.isbn = lcr.libro JOIN Raccolta AS r ON lcr.raccolta = r.cod_raccolta
-        WHERE lcr.raccolta = cur_raccolta AND (r.visibilita='pubblica' OR r.proprietario = cur_utente)
+        WHERE lcr.raccolta = raccolta_richiesta AND (r.visibilita='pubblica' OR r.proprietario = utente_richiedente)
     );
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION ArticoliInRivista(cur_rivista Rivista.issn%type)
+CREATE OR REPLACE FUNCTION ArticoliInRivista(rivista_richiesta Rivista.issn%TYPE)
 RETURNS TABLE (doi Articolo_Scientifico.doi%TYPE, titolo Articolo_Scientifico.titolo%TYPE) AS
 $$
 BEGIN
@@ -45,68 +45,68 @@ BEGIN
     );
 END;
 $$ LANGUAGE plpgsql;
-    
-CREATE OR REPLACE FUNCTION ArticoliInConferenza(cur_conferenza Conferenza.cod_conferenza%type)
+
+CREATE OR REPLACE FUNCTION ArticoliInConferenza(conferenza_richiesta Conferenza.cod_conferenza%TYPE)
 RETURNS TABLE (doi Articolo_Scientifico.doi%TYPE, titolo Articolo_Scientifico.titolo%TYPE) AS
 $$
 BEGIN
     RETURN QUERY(
         SELECT a.doi, a.titolo
         FROM Presentazione_Articolo AS pa JOIN Articolo_Scientifico AS a ON pa.articolo_scientifico = a.doi
-        WHERE pa.conferenza = cur_conferenza
+        WHERE pa.conferenza = conferenza_richiesta
     );
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION LibriDaAutore(cur_autore Autore.cod_autore%type)
+CREATE OR REPLACE FUNCTION LibriDaAutore(autore_richiesto Autore.cod_autore%TYPE)
 RETURNS TABLE (isbn Libro.isbn%TYPE, titolo Libro.titolo%TYPE) AS
 $$
 BEGIN
     RETURN QUERY(
         SELECT l.isbn, l.titolo
         FROM Scrittura_Libro AS sl JOIN Libro AS l ON sl.libro = l.isbn
-        WHERE sl.autore = cur_autore
+        WHERE sl.autore = autore_richiesto
     );
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION ArticoloDaAutore(cur_autore Autore.cod_autore%type)
+CREATE OR REPLACE FUNCTION ArticoloDaAutore(autore_richiesto Autore.cod_autore%TYPE)
 RETURNS TABLE (doi Articolo_Scientifico.doi%TYPE, titolo Articolo_Scientifico.titolo%TYPE) AS
 $$
 BEGIN
     RETURN QUERY(
         SELECT a.doi, a.titolo
         FROM Scrittura_Articolo AS sa JOIN Articolo_Scientifico AS a ON sa.articolo_scientifico = a.doi
-        WHERE sa.autore = cur_autore
+        WHERE sa.autore = autore_richiesto
     );
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION NegoziDaLibro(cur_libro Libro.ISBN%type)
+CREATE OR REPLACE FUNCTION NegoziDaLibro(libro_richiesto Libro.ISBN%TYPE)
 RETURNS TABLE (partita_iva Negozio.partita_iva%TYPE, nome Negozio.nome%TYPE) AS
 $$
 BEGIN
     RETURN QUERY(
         SELECT n.partita_iva, n.nome
         FROM Vendita AS v JOIN Negozio AS n ON v.negozio = n.partita_iva
-        WHERE v.libro = cur_libro
+        WHERE v.libro = libro_richiesto
     );
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION LibriInSala(cur_sala Sala.cod_sala%type)
+CREATE OR REPLACE FUNCTION LibriInSala(sala_richiesta Sala.cod_sala%TYPE)
 RETURNS TABLE (isbn Libro.isbn%TYPE, titolo Libro.titolo%TYPE) AS
 $$
 BEGIN
     RETURN QUERY(
         SELECT l.isbn, l.titolo
         FROM Libro AS l JOIN Presentazione_Libro AS p ON l.isbn = p.libro
-        WHERE p.sala = cur_sala
+        WHERE p.sala = sala_richiesta
     );
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION LibriInCollana(cur_collana Collana.issn%type)
+CREATE OR REPLACE FUNCTION LibriInCollana(collana_richiesta Collana.issn%TYPE)
 RETURNS TABLE (isbn Libro.isbn%TYPE, titolo Libro.titolo%TYPE) AS
 $$
 BEGIN
@@ -118,14 +118,14 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION LibriInSerie(cur_serie Serie.nome%type)
+CREATE OR REPLACE FUNCTION LibriInSerie(serie_richiesta Serie.nome%TYPE)
 RETURNS TABLE (isbn Libro.isbn%TYPE, titolo Libro.titolo%TYPE) AS
 $$
 BEGIN
         RETURN QUERY(
             SELECT l.isbn, l.titolo
             FROM Libro AS l JOIN Serie AS s ON (s.sequel=l.isbn OR s.prequel=l.isbn)
-            WHERE s.nome = cur_serie
+            WHERE s.nome = serie_richiesta
         );
 END;
 $$ LANGUAGE plpgsql;
@@ -137,12 +137,18 @@ ORDER BY calcoloNumeroSalvataggiRaccolta(r.cod_raccolta) DESC
 FETCH FIRST 10 ROWS ONLY;
 
 CREATE OR REPLACE VIEW NegoziConSerieComplete AS
-SELECT DISTINCT v.negozio, n.indirizzo, n.url, s.nome
-FROM (Serie AS s JOIN Vendita AS v ON s.prequel = v.libro) JOIN Negozio AS n ON v.negozio = n.partita_iva
-WHERE NOT EXISTS (
-    SELECT s1.sequel, s1.prequel
+SELECT DISTINCT s.nome AS nome_serie, n.partita_iva, n.nome AS nome_negozio, n.indirizzo, n.url
+FROM (Serie AS s JOIN Vendita AS v ON (s.prequel = v.libro OR s.sequel = v.libro)) JOIN Negozio AS n ON v.negozio = n.partita_iva
+AND NOT EXISTS (
+    SELECT *
     FROM serie AS s1
     WHERE s1.nome = s.nome
-    AND (sequel NOT IN (SELECT libro FROM Vendita AS v1 WHERE v1.negozio = v.negozio)
-    OR libro NOT IN (SELECT libro FROM Vendita AS v1 WHERE v1.negozio = v.negozio))
+    AND (s1.sequel NOT IN (SELECT libro FROM Vendita AS v1 WHERE v1.negozio = v.negozio)
+    OR s1.prequel NOT IN (SELECT libro FROM Vendita AS v1 WHERE v1.negozio = v.negozio))
+)
+AND NOT EXISTS (
+    SELECT *
+    FROM Serie AS s1 JOIN Vendita AS v1 ON (s1.prequel = v1.libro OR s1.sequel = v1.libro)
+    WHERE v1.negozio = v.negozio
+    AND v1.quantita = 0
 );
